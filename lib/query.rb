be846a599c3932e6cf58b412be1dd4823365d408
RajaRaviVarma A R <ravi4frenz@gmail.com>
2016-10-17 02:33:18 +0530

    Finishing Query functionality.

--- lib/query.rb
+++ lib/query.rb
@@ -1,9 +1,12 @@
-class Query
+require 'digest/sha1'
 
+require 'hasher.rb'
+
+class Query
 
   class InvalidParams < StandardError; end
 
-  SEPARATOR = '|'
+  SEPERATOR = '|'
   REQUIRED_FIELDS = %i[bank_ifsc_code bank_account_number amount merchant_transaction_ref
                      transaction_date payment_gateway_merchant_reference].sort
   def initialize(params)
@@ -12,19 +15,23 @@ class Query
     sanitize_params!
   end
 
-  def payload
+  def payload_to_payment_gateway
+    EncryptService.new(payload_with_sha).encrypt
   end
 
   private
 
-  def hash
+  def payload_with_sha
+    plain_string = build_string
+    "#{plain_string}|hash=#{Hasher.digest(plain_string)}"
   end
 
-  def plain_string
-  end
-
-  def encrypt
-    EncryptService.new(hash)
+  def build_string
+    fields = []
+    @params.each do |field, value|
+      fields << "#{field}=#{value}"
+    end
+    fields.join(SEPERATOR)
   end
 
   def validate!
--- spec/lib/query_spec.rb
+++ spec/lib/query_spec.rb
@@ -4,7 +4,7 @@ describe Query do
 
   let(:bank_ifsc_code) { 'ICIC0000001' }
   let(:bank_account_number) { 11111111 }
-  let(:amount) { 10000.00 }
+  let(:amount) { 10000.0 }
   let(:merchant_transaction_ref) { 'txn001' }
   let(:transaction_date) { '2014-11-14' }
   let(:payment_gateway_merchant_reference) { 'merc001' }
@@ -14,6 +14,7 @@ describe Query do
                        merchant_transaction_ref: merchant_transaction_ref,
                        transaction_date: transaction_date,
                        payment_gateway_merchant_reference: payment_gateway_merchant_reference }}
+  let(:query) { described_class.new(query_params) }
 
   describe '#initialize' do
     it 'takes all necessary parameters to build the query' do
@@ -29,6 +30,36 @@ describe Query do
       unwanted_params = query_params.update(unwanted_field: 2435)
       query = described_class.new(unwanted_params)
       expect(query.instance_variable_get(:@params).keys).not_to include(:unwanted_field)
+    end
+  end
+
+  describe '#payload_to_payment_gateway' do
+    it 'encrypts the payload' do
+      payload_with_sha = 'payload with sha'
+      encrypt_service = double('EncryptService', encrypt: 'scrambled text')
+      expect(query).to receive(:payload_with_sha).and_return(payload_with_sha)
+      expect(encrypt_service).to receive(:encrypt)
+      expect(EncryptService).to receive(:new).with(payload_with_sha).and_return(encrypt_service)
+      query.payload_to_payment_gateway
+    end
+  end
+
+  # private methods
+  describe '#build_string' do
+    it "returns #{Query::SEPERATOR} seperated field=value string" do
+      expect(query.send(:build_string)).to eql('bank_ifsc_code=ICIC0000001|bank_account_number=11111111|'\
+                                               'amount=10000.0|merchant_transaction_ref=txn001|'\
+                                               'transaction_date=2014-11-14|payment_gateway_merchant_reference'\
+                                               '=merc001')
+    end
+  end
+
+  describe '#payload_with_sha' do
+    it "returns #{Query::SEPERATOR} seperated string with hash=sha(build_string)" do
+      sample_query_string = 'one=1'
+      expect(query).to receive(:build_string).and_return(sample_query_string)
+      expect(query.send(:payload_with_sha)).
+        to eql("#{sample_query_string}|hash=#{Hasher.digest(sample_query_string)}")
     end
   end
 end
