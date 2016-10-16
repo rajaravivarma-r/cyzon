8c593738b452c138d59ca8588412058ec222416d
RajaRaviVarma A R <ravi4frenz@gmail.com>
2016-10-17 04:07:04 +0530

    Mock class for PayGateway data

---
+++ lib/payment_gateway.rb
@@ -0,0 +1,8 @@
+class PaymentGateway
+
+  RESPONSE = 'txn_status=success|amount=10000.00|' \
+             'merchant_transaction_ref=txn001|transaction_date=2014-11-14|'\
+             'payment_gateway_merchant_reference=merc001|'\
+             'payment_gateway_transaction_reference=pg_txn_0001|'\
+             'hash=abdedffduedd000000988'
+end
