fcc663d0e5fcb05f3f5f51f91252c74d7fa12be7
RajaRaviVarma A R <ravi4frenz@gmail.com>
2016-10-17 13:55:31 +0530

    Fixing reek warnings

--- lib/query.rb
+++ lib/query.rb
@@ -3,16 +3,18 @@ require 'base64'
 
 require 'hasher.rb'
 
+# Class which encapsulated query building and making it suitable for the Payment Gateway
 class Query
 
+  # Exception thrown when the passed in params are invalid
   class InvalidParams < StandardError; end
 
   REQUIRED_FIELDS = %i[bank_ifsc_code bank_account_number amount merchant_transaction_ref
                      transaction_date payment_gateway_merchant_reference].sort
   def initialize(params)
     @params = params
-    validate!
-    sanitize_params!
+    validate
+    sanitize_params
   end
 
   def payload_to_payment_gateway
@@ -30,14 +32,14 @@ class Query
     EncryptService.new(payload_with_sha).encrypt
   end
 
-  def validate!
+  def validate
     missing_keys = REQUIRED_FIELDS - @params.keys.sort
     if missing_keys.any?
       raise InvalidParams.new("missing keys: #{missing_keys.join(', ')}")
     end
   end
 
-  def sanitize_params!
+  def sanitize_params
     @params.reject! { |key, val| !REQUIRED_FIELDS.include?(key) }
   end
 end
